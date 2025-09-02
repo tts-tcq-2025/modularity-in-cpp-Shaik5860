df7c484c78ca198d703c00c7f8b892dfb2daa059
Shaik5860 <waseemahammad.shaik@gmail.com>
2025-09-02 13:20:40 +0530

    Update ColorPair.h

--- ColorPair.h
+++ ColorPair.h
@@ -3,27 +3,28 @@
 
 #include <string>
 
-namespace TelCoColorCoder
-{
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+namespace TelCoColorCoder {
 
-    extern const char* MajorColorNames[];
-    extern const char* MinorColorNames[];
-    extern int numberOfMajorColors;
-    extern int numberOfMinorColors;
+enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
-    class ColorPair {
-    private:
-        MajorColor majorColor;
-        MinorColor minorColor;
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern const int numberOfMajorColors;
+extern const int numberOfMinorColors;
 
-    public:
-        ColorPair(MajorColor major, MinorColor minor);
-        MajorColor getMajor() const;
-        MinorColor getMinor() const;
-        std::string ToString() const;
-    };
-}
+class ColorPair {
+ public:
+    ColorPair(MajorColor major, MinorColor minor);
+    MajorColor get_major() const;
+    MinorColor get_minor() const;
+    std::string to_string() const;
 
-#endif
+ private:
+    MajorColor major_;
+    MinorColor minor_;
+};
+
+} // namespace TelCoColorCoder
+
+#endif // COLORPAIR_H
