eebf5e89aea6344c66f226bb25f3963a13422fbd
Shaik5860 <waseemahammad.shaik@gmail.com>
2025-09-02 13:25:00 +0530

    Update Tests.cpp

--- Tests.cpp
+++ Tests.cpp
@@ -1,33 +1,32 @@
+#include "ColorUtils.h"
 #include <iostream>
 #include <assert.h>
-#include "ColorUtils.h"
 
-namespace TelCoColorCoder
-{
-    void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
-        ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-        std::cout << "[TEST] Pair Number " << pairNumber 
-                  << " -> " << colorPair.ToString() << std::endl;
-        assert(colorPair.getMajor() == expectedMajor);
-        assert(colorPair.getMinor() == expectedMinor);
-    }
+namespace TelCoColorCoder {
 
-    void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
-        int pairNumber = GetPairNumberFromColor(major, minor);
-        std::cout << "[TEST] Colors " << MajorColorNames[major] << " " << MinorColorNames[minor]
-                  << " -> Pair Number " << pairNumber << std::endl;
-        assert(pairNumber == expectedPairNumber);
-    }
-
-    void RunAllTests() {
-        std::cout << "----- Running Unit Tests -----\n";
-
-        testNumberToPair(4, WHITE, BROWN);
-        testNumberToPair(5, WHITE, SLATE);
-
-        testPairToNumber(BLACK, ORANGE, 12);
-        testPairToNumber(VIOLET, SLATE, 25);
-
-        std::cout << "----- All Tests Passed ✅ -----\n\n";
-    }
+static void test_number_to_pair(int pair_number, MajorColor expected_major,
+                                MinorColor expected_minor) {
+    ColorPair cp = get_color_from_pair_number(pair_number);
+    std::cout << "[TEST] Pair " << pair_number << " -> " << cp.to_string() << "\n";
+    assert(cp.get_major() == expected_major);
+    assert(cp.get_minor() == expected_minor);
 }
+
+static void test_pair_to_number(MajorColor major, MinorColor minor,
+                                int expected_pair_number) {
+    int pair_number = get_pair_number_from_color(major, minor);
+    std::cout << "[TEST] Colors " << MajorColorNames[major] << " "
+              << MinorColorNames[minor] << " -> Pair " << pair_number << "\n";
+    assert(pair_number == expected_pair_number);
+}
+
+void RunAllTests() {
+    std::cout << "----- Running Unit Tests -----\n";
+    test_number_to_pair(4, WHITE, BROWN);
+    test_number_to_pair(5, WHITE, SLATE);
+    test_pair_to_number(BLACK, ORANGE, 12);
+    test_pair_to_number(VIOLET, SLATE, 25);
+    std::cout << "----- All Tests Passed ✅ -----\n\n";
+}
+
+} // namespace TelCoColorCoder
